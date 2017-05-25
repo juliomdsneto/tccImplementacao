3c041890120e144f87603909cb42d926f7bb73ff
juliomachado <jmdsneto@inf.ufpel.edu.br>
2017-05-24 23:06:41 -0300

    e la vamos nos...

---
+++ lib/QuantummProjectiveMeasure.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <complex.h>
+#include <vector>
+#include <map>
+
+void q_ProjectiveMeasureResolve(float complex *state, int numqubits, std::vector<int> qubits_to_measure);
