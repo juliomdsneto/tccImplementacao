032120b23a7339f4b69148e3388d3390c87e5587
juliomdsneto <juliomdsneto@gmail.com>
2017-07-20 16:48:10 -0300

    adicionando alguns dos graficos

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	g++ -Wall -Werror -O3 -fcx-limited-range lib/*.cpp src/*.cpp -lgsl -lgslcblas -lm -fopenmp -o main_4t
+	g++ -Wall -Werror -O3 -fcx-limited-range lib/*.cpp src/*.cpp -lgsl -lgslcblas -lm -fopenmp -o main_seq
 
 run:
 	./main
--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 GSL_RNG_SEED=$(date +%s%N) GSL_RNG_TYPE=mrg
-./main_4t > saida4T.txt;
-./main_2t > saida2T.txt;
-./main_1t > saida1.Ttxt;
+./main_seq > saida_seq.txt
+#./main_4t > saida4T.txt;
+#./main_2t > saida2T.txt;
+#./main_1t > saida1.Ttxt;
 
 
 
